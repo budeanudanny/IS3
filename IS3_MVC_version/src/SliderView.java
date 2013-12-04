bd71f8b85cf6c22acbc2f9d1560f9cf6e54c9f3d
danicretu <cretu_daniela92@yahoo.com>
2013-12-04 13:22:12 +0000

    slider view updates

--- IS3_MVC_version/src/SliderView.java
+++ IS3_MVC_version/src/SliderView.java
@@ -8,11 +8,13 @@ import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class SliderView extends JPanel {
 	
-    JSlider jSlider1;
-    JSlider  jSlider2;
+    RangeSlider jSlider1;
+    RangeSlider  jSlider2;
     Model model;
     
     
@@ -24,11 +26,15 @@ public class SliderView extends JPanel {
     	jSlider1.setMajorTickSpacing(0);
     	jSlider1.setPaintLabels(true);
     	jSlider1.setPaintTicks(true);
+    	jSlider1.addChangeListener(new Modified());
+    	jSlider1.setName("slider1");
         jSlider2 = new RangeSlider();
         jSlider2.setMajorTickSpacing(0);
 		jSlider2.setPaintTicks(true);
         jSlider2.setPaintLabels(true);
-        
+    	jSlider2.addChangeListener(new Modified());
+    	jSlider2.setName("slider2");
+    	
         this.add(jSlider1);
         this.add(jSlider2);
         
@@ -36,22 +42,10 @@ public class SliderView extends JPanel {
         
     }
     
-    private class Modified implements ActionListener{
-    	double min = -1D,max = -1D;
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JSlider s = (JSlider) e.getSource();
-			if (s.getName().equals(jSlider1)){
-				min = s.getValue();
-			}else if (s.getName().equals(jSlider2)){
-				max = s.getValue();
-			}
-			
-		}
+   
     
     
     
-    }
     
     public void update(String box, int value){
     	int min=0;
@@ -67,8 +61,8 @@ public class SliderView extends JPanel {
     	}
     	else if (value == 1){
     		min = 0;
-    		max= 70000;
-    		interval = 10;
+    		max= 70;	//70000
+    		interval = 10;	//70000
     	}
     	else if (value ==2){
     		min = 0;
@@ -116,13 +110,13 @@ public class SliderView extends JPanel {
     		interval = 10;
     	}
     	else if (value == 11){
-    		System.out.println("here");
+ 
     		min = 0;
     		max= 40;
     		interval = 10;
     	}
     	else if (value ==12){
-    		System.out.println("here");
+
     		min = 0;
     		max= 120;
     		interval = 20;
@@ -145,10 +139,35 @@ public class SliderView extends JPanel {
     		jSlider2.setMaximum(max);
     		jSlider2.setMajorTickSpacing(interval);
        		jSlider2.setPaintTicks(true);
-        	//jSlider2.createStandardLabels(50);
+    		jSlider2.setPaintLabels(true);
+
     	}
     }
     
     
+    private class Modified implements ChangeListener{
+    	double min = -1D,max = -1D;
+
+		public void stateChanged(ChangeEvent e) {
+
+			RangeSlider s = (RangeSlider) e.getSource();
+
+			if (s.getValue() == s.getUpperValue())
+			{
+
+			}
+			else{
+				if (s.getName().equals("slider1")){
+					min = s.getValue();
+					max = s.getUpperValue();
+				}else if (s.getName().equals(jSlider2)){
+					min = s.getValue();
+					max = s.getUpperValue();
+				}
+			// TODO Auto-generated method stub
+			}
+		}
+    
+    } 
 
 }
